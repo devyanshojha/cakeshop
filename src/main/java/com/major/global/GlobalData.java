b3cf4a625c9649013dada6f3eca76deaedfde3db
devyanshojha <ojhadevyansh@gmail.com>
2021-08-25 06:29:14 +0530

    Added new features

---
+++ src/main/java/com/major/global/GlobalData.java
@@ -0,0 +1,14 @@
+package com.major.global;
+
+import com.major.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData {
+	public static List<Product> cart;
+
+	static {
+			cart=new ArrayList<Product>();
+	}
+}
